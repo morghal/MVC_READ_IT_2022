c503337d41926dca3b17e65aa981c51828fb3777
Morgan <morganhalin@gmail.com>
2022-05-12 13:56:11 +0200

    Decoupe Template

---
+++ app/views/templates/partials/chvj.php
@@ -0,0 +1,193 @@
+<div class="row d-flex">
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_1.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_2.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_3.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_4.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_5.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_6.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_7.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_8.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6 d-flex ftco-animate">
+                	<div class="blog-entry justify-content-end">
+                    <a href="article.html" class="block-20" style="background-image: url('assets/images/image_9.jpg');">
+                    </a>
+                    <div class="text p-4 float-right d-block">
+                    	<div class="topper d-flex align-items-center">
+                    		<div class="one py-2 pl-3 pr-1 align-self-stretch">
+                    			<span class="day">18</span>
+                    		</div>
+                    		<div class="two pl-0 pr-3 py-2 align-self-stretch">
+                    			<span class="yr">2019</span>
+                    			<span class="mos">October</span>
+                    		</div>
+                    	</div>
+                    	<h3 class="heading mb-3"><a href="#">Article title</a></h3>
+                      <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                      <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row mt-5">
+                <div class="col text-center">
+                  <div class="block-27">
+                    <ul>
+                      <li><a href="#">+</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
